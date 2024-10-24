9c3b28f9a863f70a850593087b6282724b47562c
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2024-10-24 16:14:57 +0530

    code

---
+++ jwt/SpringSecurityJwt2/src/main/java/com/example/demo/UserData.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserData {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int uid;
+	private String name;
+	private String email;
+	private String password;
+	private String roles;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRoles() {
+		return roles;
+	}
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+	public UserData() {
+		// TODO Auto-generated constructor stub
+	}
+	public UserData(int uid, String name, String email, String password, String roles) {
+		this.uid = uid;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.roles = roles;
+	}
+	
+	
+}
